9c2654139c77e1478946ed7d1679b72e346cb5bd
Dilnura [FE17] <102959751+Dilnura-d@users.noreply.github.com>
2023-01-23 15:29:44 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+
+function setInfo()
+ {
+   
+let student1 = {
+    name:"John",
+   object: {student1:""
+   }}
+   console.log(student1,student1.name.length);
+
+   let student2 = {
+    name:"Alex",
+   object: {student2:""
+   }}
+   console.log(student2,student2.name.length);
+
+   let student3 = {
+    name:"Alice",
+   object: {student3:""
+   }}
+   console.log(student3,student3.name.length);
+
+   let student4 = {
+    name:"Maria",
+   object: {student4:""
+   }}
+   console.log(student4,student4.name.length);
+
+   let student5 = {
+    name:"Aidan",
+   object: {student5:""
+   }}
+   console.log(student5,student5.name.length);
+
+   let student6 = {
+    name:"Justin",
+   object: {student6:""
+   }}
+   console.log(student6,student6.name.length);
+
+   let student7 = {
+    name:"Oliver",
+   object: {student7:""
+   }}
+   console.log(student7,student7.name.length);
+
+   let student8 = {
+    name:"Alberto",
+   object: {student8:""
+   }}
+   console.log(student8,student8.name.length);
+
+   let student9 = {
+    name:"Charlie",
+   object: {student9:""
+   }}
+   console.log(student9,student9.name.length);
+
+   let student10 = {
+    name:"Maddison",
+   object: {student10:""
+   }}
+   console.log(student10,student10.name.length);
+
+
+}
+
+setInfo()
+
